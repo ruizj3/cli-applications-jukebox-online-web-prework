2ed8c57ecc02b12a68b7013d17bcdf533c79ac2d
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-18 02:05:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,6 +19,12 @@ def help
   puts "- exit : exits this program"
 end
 
+def list(songs)
+songs.each_with_index {|song, index|
+  puts "#{index+1}. #{song}"
+}
+end
+
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.chomp
