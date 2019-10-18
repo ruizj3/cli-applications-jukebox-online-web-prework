ce4790ba66ec863fb0d20e2694df328aa2c2b7b6
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-18 02:10:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,4 +39,24 @@ end
 
 def exit_jukebox
   puts "Goodbye"
-end 
+end
+
+def run(songs)
+  while true do
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
+    end
+  end
+end
